495aa6efbd7b13da0efed6866ce45b46236faf6b
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-03-27 15:01:31 +0300

    updates data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,34 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts"Start seeding..."
+puts "Seeding database...."
+#Adds data to heros
+Hero.create(name:"John Doe",super_name:"SoupaMan")
+Hero.create(name:"Tom Harry",super_name:"Tomato")
+Hero.create(name:"Billy Jean",super_name:"Best Jeanist")
 
-powers = [
-  {name:"super strength",description: "gives the wielder super human strengths"},
-  {name:"flight",description: "gives the wielder the ability to fly through the skys at supersonic speed"},
-  {name:"super human senses",description: "allows the person to user their senses at super human level"},
-  {name:"elasticity",description: "can stretch the human body to extreme lengths"}
-]
+#Adds data to powers
+Power.create(name:"Smokes",description:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+Power.create(name:"Jokes",description:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+Power.create(name:"Lokes",description:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+Power.create(name:"Blokes",description:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+Power.create(name:"Frokes",description:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+Power.create(name:"Quokes",description:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
 
-puts "creating powers..."
+#Adds data to hero_powers
+HeroPower.create(strength:"Weak",power_id:1,hero_id:1)
+HeroPower.create(strength:"Strong",power_id:2,hero_id:1)
+HeroPower.create(strength:"Medium",power_id:3,hero_id:1)
+HeroPower.create(strength:"Weak",power_id:4,hero_id:2)
+HeroPower.create(strength:"Strong",power_id:5,hero_id:3)
+HeroPower.create(strength:"Medium",power_id:6,hero_id:2)
 
-powers.each { |power| Power.create(power) }
 
-heros = [
-  {name:"Peter Parker", super_name:"Spider-Man"},
-  {name:"Steve Rogers", super_name:"Captain America"},
-  {name:"Bruce Banner", super_name:"Hulk"},
-  {name:"Natasha Romanoff", super_name:"Black Widow"},
-  {name:"Matthew Murdock", super_name:"Dare Devil"},
-  {name:"Reed Richards", super_name:"Mr.Fantastic"},
-  {name:"T'Challa", super_name:"Black Panther"},
-  {name:"Thor Odinson", super_name:"Thor"},
-  {name:"Tony Stark", super_name:"Iron Man"},
-  {name:"Carol Danvers", super_name:"Captain Marvel"} 
-]
-
-puts "creating heroes..."
-
-heros.each { |hero| Hero.create(hero) }
-
-puts "Done."
+puts "Completed seeding!"
