f9eda160410ad94ad94fe586b45b98fded4dab84
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-03-28 14:41:56 +0300

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :powers, only: [:index, :show, :update]
   # resources :heros, only: [:index, :show]
 
-  get '/heroes', to: 'heros#index'
-  get '/heroes/:id', to: 'heros#show'
+  get '/heros', to: 'heros#index'
+  get '/heros/:id', to: 'heros#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
